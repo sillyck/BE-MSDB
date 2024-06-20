92d1fe09c49773092fb1df95274f434c0bf192cf
Jordi Ribellas <ribellas2002@gmail.com>
2024-06-20 12:13:55 +0200

    Principi de Register, Login, JWT...

---
+++ src/main/java/com/api/demo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.api.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		return http
+	}
+}
